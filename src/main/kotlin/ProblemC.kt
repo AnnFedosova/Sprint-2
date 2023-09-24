b45634c5ae957423b6fa1c7b6a21b4e4aaa6e536
Levina <annaLevinaM@yandex.ru>
2023-09-24 23:30:00 +0300

    решение задач A,B,C

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,22 @@
 class ProblemC {
 
     fun main() {
+        val inputList = this::class.java.getResourceAsStream("problemC/input.txt")!!
+            .bufferedReader().readLines()
+        val studentsLevelList: List<Int> = inputList[1].trim().splitToSequence(' ')
+            .toList()
+            .stream()
+            .map { a -> a.toInt() }
+            .sorted()
+            .toList()
 
-        //ваша реализация в методе main
-
+        var resultTaskCount = 0 //кол-во задач, которые нужно решить группе студентов
+        var i = 0
+        while (i < studentsLevelList.size) {
+            resultTaskCount += studentsLevelList[i + 1] - studentsLevelList[i]
+            i += 2
+        }
+        println(resultTaskCount)
     }
 
 }
